b50d7517d785cc896723b7dfab5d62e6205e3aa9
Vinoth <vinothkumar0549@gmail.com>
2023-12-28 14:08:29 +0530

    Files Added

---
+++ js/config/config.js
@@ -0,0 +1,17 @@
+/**
+ * @file config.js
+ * @author Sanjay Sunil
+ * @license MIT
+ */
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1_AmD3kuhewL1UkVY23V_8yNB5ZO5pfE",
+  authDomain: "login-f76f2.firebaseapp.com",
+  projectId: "login-f76f2",
+  storageBucket: "login-f76f2.appspot.com",
+  messagingSenderId: "116960867955",
+  appId: "1:116960867955:web:665b28dde590fa7e951c06",
+  measurementId: "G-63VWEW6RDJ",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
